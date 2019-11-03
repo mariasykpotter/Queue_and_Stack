35df18e9d573af2c7cee8d73ddb2764d9df69618
mariasykpotter <mpetrivska21@gmail.com>
2019-11-03 19:58:32 +0200

    'fdg'

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,26 +1,5 @@
 package ua.edu.ucu.collections;
 
-import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
-
 public class Stack {
-    private ImmutableLinkedList LinkedList;
-
-    public Stack() {
-        LinkedList = new ImmutableLinkedList();
-    }
-
-    Object peek() {
-        return LinkedList.getFirst();
-    }
-
-    Object pop() {
-        Object firstEl = LinkedList.getFirst();
-        LinkedList = LinkedList.removeFirst();
-        return firstEl;
-    }
-
-    void push(Object e) {
-        LinkedList = LinkedList.addFirst(e);
-    }
-
+    
 }
